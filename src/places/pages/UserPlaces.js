3d2fb53aa0f940d2661688ebb6dbd3c779dcf3df
SlavenSaric <slavendownload@gmail.com>
2023-10-09 17:38:21 +0200

    login form added

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -5,7 +5,7 @@ import PlaceList from "../components/PlaceList";
 
 export const DUMMY_PLACES = [
     {id: 'p1',
-    title: 'Empire state Building new york', description: 'One of the most famous sky scrapers in the world.', image: 'https://image.stern.de/30507262/t/kK/v2/w1440/r1/-/empire-state-building.jpg', address: '20 W 34th St., New York, NY 10001, USA',
+    title: 'Empire State Building', description: 'One of the most famous sky scrapers in the world.', image: 'https://image.stern.de/30507262/t/kK/v2/w1440/r1/-/empire-state-building.jpg', address: '20 W 34th St., New York, NY 10001, USA',
     location: {
         lat: '40.7484405',
         long: '-73.9856644'
---
+++ src/user/pages/Auth.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+import "./Auth.css";
+import Button from "../../shared/components/FormElements/Button";
+import Input from "../../shared/components/FormElements/Input";
+import Card from "../../shared/components/UIElements/Card";
+import {
+  VALIDATOR_EMAIL,
+  VALIDATOR_MINLENGTH,
+} from "../../shared/util/validators";
+import {useForm} from '../../shared/hooks/form-hook'
+
+const Auth = () => {
+
+  const [formState, inputHandler] = useForm({
+    email: {
+      value: '',
+      isValid: false
+    },
+    password: {
+      value: '',
+      isValid: false
+    }
+  }, false)
+
+  const AuthSubmitHandler =(e) => {
+    e.preventDefault()
+    console.log(formState.inputs);
+  }
+
+  return (
+    <Card className="authentication padd">
+      <h2>Login Required</h2>
+      <hr />
+      <form onSubmit={AuthSubmitHandler}>
+        <Input
+          id="email"
+          element="input"
+          type="email"
+          label="E-mail"
+          validators={[VALIDATOR_EMAIL()]}
+          errorText="Please entere a valid email address."
+          onInput={inputHandler}
+        />
+        <Input
+          id="password"
+          element="input"
+          type="password"
+          label="Password"
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText="Please entere a valid password."
+          onInput={inputHandler}
+        />
+        <Button type="submit" disabled={!formState.isValid}>LOGIN</Button>
+      </form>
+    </Card>
+  );
+};
+
+export default Auth;
