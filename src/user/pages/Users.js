759616e12211b3375b6c040d3451b2bd502ebfbe
SlavenSaric <slavendownload@gmail.com>
2023-10-21 14:20:43 +0200

    getting users with a GET request

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,18 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UsersList from "../components/UsersList";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
 const Users = () => {
-  const USERS = [
-    {
-      id: "u1",
-      name: "Max",
-      image:
-        "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      places: 3,
-    },
-  ];
+  const [iseLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(false)
+  const [loadedUsers, setLoadedUsers] = useState(false)
 
-  return <UsersList items={USERS} />;
+
+  useEffect(()=> {
+    const sendRequest = async () => {
+      setIsLoading(true)
+      try{
+        const response = await fetch('http://localhost:5000/api/users')
+        const responseData = await response.json()
+
+        if(!response.ok){
+          throw new Error(responseData.message)
+        }
+
+        setLoadedUsers(responseData.users)
+        console.log(responseData);
+    }catch(err){
+      setError(err.message)
+    }
+    setIsLoading(false)
+    }
+    sendRequest()
+  },[])
+
+  const errorHandler = () => {
+    setError(null)
+  }
+
+
+  return <>
+  <ErrorModal error={error} onClear={errorHandler} />
+  {iseLoading && (
+    <div className="center">
+      <LoadingSpinner />
+    </div>
+  )}
+  {!iseLoading && loadedUsers && <UsersList items={loadedUsers} />}
+  </>;
 };
 
 export default Users;
